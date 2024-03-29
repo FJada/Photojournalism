281ade549cab952e63d4c7ca07e43910cfa01098
FJada <124617739+FJada@users.noreply.github.com>
2024-03-29 06:06:37 -0400

    about page

--- game-web/src/components/About/AboutPage.jsx
+++ game-web/src/components/About/AboutPage.jsx
@@ -25,17 +25,27 @@ const About = () => {
               gaming content and gain followers. In a paper, The new third place: Massively multiplayer online gaming in American youth culture by Constance Steinkuehler, she argues "that massively multiplayer online games (MMOGs) 
               function as one novelform of a new “third place” for informal sociability. 
               Based on data collected as part of an ongoing two-year  virtual   cognitive ethnography of the game  Lineage  (first  I,  now II), I outline how  the features ofMMOG digital worlds satisfy Oldenburg’s (1999) defining criteria for the very sorts of third places “realworld” America sorely lacks.""
-              "
-
-              
+              Why online gaming specifically, Andrew Fishman addresses this in an article "Why So Many Teens Use Video Games to Meet Others", and claims that some causes are due to a lack of accessibility in reaching physical thrid spaces, social anxiety, and digital customization of personas allowing young people to reach many
+               other people from the comfort of their own home on their terms. In the Forbes article, "The Third Space", by Mark Nevins he states that people should be intentional with the incorporation of engaging in your thirdspace, using gaming people can develop their skills in their respective games. 
             </p>
 
             <p>Sources: <a href="https://www.brookings.edu/articles/third-places-as-community-builders/">Brookings</a>
              </p>
+
             <p>
             <a href="https://www.researchgate.net/publication/288865331_The_new_third_place_Massively_multiplayer_online_gaming_in_American_youth_culture"> Constance Steinkuehler </a>
             </p>
-            
+            <p>
+            <a href="https://www.psychologytoday.com/us/blog/video-game-health/202303/why-so-many-teens-use-video-games-to-meet-others">Andrew Fishman  </a>
+            </p>
+
+            <p>
+            <a href="https://www.forbes.com/sites/hillennevins/2021/10/14/the-third-space/?sh=3a84bbfd686a">Forbes </a>
+            </p>
+            <p>
+            <a href="https://academic.oup.com/jcmc/article/11/4/885/4617703">Where Everybody Knows Your (Screen) Name: Online Games as “Third Places”</a>
+            </p>
+
           </div>
         </div>
       </MiniBrowser>
